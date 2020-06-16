b83d2f99fe1f80f014f4f054664a8d5afca6193a
Kirty Bol <kwobol@icloud.com>
2020-06-16 11:46:20 +0200

    Comments added

--- Code/Write.py
+++ Code/Write.py
@@ -1,12 +1,17 @@
+# Libraries
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 
+# Reader simplification
 reader = SimpleMFRC522()
 
+# To test if it works
 try:
+	# Type data to write on the tag/card
 	text = input('New data: ')
 	print("Hold tag at reader")
 	reader.write(text)
+	# Confirmation it worked
 	print("Written")
 
 finally:
